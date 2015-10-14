d9d2c19797693ac44e1199a65d2f961fa2375e96
David Yurek <david.a.yurek@gmail.com>
2015-10-14 04:42:02 -0400

    remove calendars resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :calendars
   resources :exemptions
   resources :people
   # The priority is based upon order of creation: first created -> highest priority.
