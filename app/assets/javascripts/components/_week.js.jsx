63f7754b256d4cc38bb7bec62340a16e2ad5c95a
David Yurek <david.a.yurek@gmail.com>
2015-10-24 19:19:00 -0400

    better naming

--- app/assets/javascripts/components/_week.js.jsx
+++ app/assets/javascripts/components/_week.js.jsx
@@ -2,11 +2,11 @@ var Week = React.createClass({
   render: function () {
     return (
       <div className="week">
-        <div className="weekday monday"></div>
-        <div className="weekday tuesday"></div>
-        <div className="weekday wednesday"></div>
-        <div className="weekday thursday"></div>
-        <div className="weekday friday"></div>
+        <div className="weekday monday-color"></div>
+        <div className="weekday tuesday-color"></div>
+        <div className="weekday wednesday-color"></div>
+        <div className="weekday thursday-color"></div>
+        <div className="weekday friday-color"></div>
       </div>
     )
   }
