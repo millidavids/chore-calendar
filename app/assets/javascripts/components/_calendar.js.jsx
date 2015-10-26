b63bdc86785ac3824e70f890a67273f6adf8972f
David Yurek <david.a.yurek@gmail.com>
2015-10-26 09:39:42 -0400

    immediately invoked function set state for calendar

--- app/assets/javascripts/components/_calendar.js.jsx
+++ app/assets/javascripts/components/_calendar.js.jsx
@@ -1,11 +1,24 @@
 var Calendar = React.createClass({
+  getInitialState: function() {
+    return {initial_day: (() => {
+      switch (this.props.current_day) {
+        case 0: return "weekend";
+        case 1: return "monday";
+        case 2: return "tuesday";
+        case 3: return "wednesday";
+        case 4: return "thursday";
+        case 5: return "friday";
+        case 6: return "weekend";
+      }
+    })()};
+  },
   render: function () {
     return (
       <div className="calendar-inner">
         <Week className="week" days={ this.props.week } />
         <div className="present-day">
           <div className="absolute-center">
-            { this.props.current_day }
+            { this.state.initial_day }
           </div>
         </div>
       </div>
