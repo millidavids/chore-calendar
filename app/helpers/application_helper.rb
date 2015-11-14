98f7c256e76c31f1d0aa5d0115a81fc1d27753bf
Sarah Vessels <cheshire137@gmail.com>
2015-11-14 12:08:07 -0500

    Reuse existing day color classes for body

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def body_class
+    classes = [params[:controller].split('/').first]
+    classes << params[:controller].split('/').join('-')
+    classes << params[:action]
+    day_name = Time.now.strftime('%A').downcase
+    if %w(saturday sunday).include? day_name
+      classes << 'weekend-color'
+    else
+      classes << "#{day_name}-color"
+    end
+    classes.join(' ')
+  end
 end
