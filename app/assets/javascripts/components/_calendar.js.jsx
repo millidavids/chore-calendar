f5dba6a8ec63e204bbd056e5f065652d1a17328b
Sarah Vessels <cheshire137@gmail.com>
2015-11-13 22:33:08 -0500

    Remove Turbolinks

--- app/assets/javascripts/components/_calendar.js.jsx
+++ app/assets/javascripts/components/_calendar.js.jsx
@@ -2,13 +2,13 @@ var Calendar = React.createClass({
   getInitialState: function() {
     return {day: (() => {
       switch (this.props.current_day) {
-        case 0: return "weekend";
-        case 1: return "monday";
-        case 2: return "tuesday";
-        case 3: return "wednesday";
-        case 4: return "thursday";
-        case 5: return "friday";
-        case 6: return "weekend";
+        case 0: return 'weekend';
+        case 1: return 'monday';
+        case 2: return 'tuesday';
+        case 3: return 'wednesday';
+        case 4: return 'thursday';
+        case 5: return 'friday';
+        case 6: return 'weekend';
       }
     })()};
   },
@@ -27,7 +27,7 @@ var Calendar = React.createClass({
       <div className="calendar">
         <div className="calendar-inner">
           <Week className="week" days={ this.props.week } onSetDay={this.setDay}/>
-          <CurrentDay day={ this.state.day } name={ name } />
+          <CurrentDay day={ this.state.day } name={ name }/>
         </div>
         <div className="managing">
           <DaySwitcher className="day-switcher" days={ this.props.week } onPeopleSwitch={this.setPerson}/>
