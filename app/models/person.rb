90dca897ba844a31d40967305fda465e76f4eab2
Sarah Vessels <cheshire137@gmail.com>
2015-11-14 11:44:19 -0500

    Tie calendar, person to a user

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,4 @@
 class Person < ActiveRecord::Base
+  belongs_to :user
   has_many :exemptions
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ActiveRecord::Base
                        if: :password_required?
   validates :password, length: {in: 6..72}
 
+  has_many :calendars
+  has_many :people
+
   private
 
   def password_required?
