92e4457bcc14ba8242c672887c946de9389d1be1
Summer Smith <jessicasummersmith@gmail.com>
2015-11-14 13:21:02 -0500

    Add warning icon to special tasks

--- app/assets/javascripts/components/_task.js.jsx
+++ app/assets/javascripts/components/_task.js.jsx
@@ -3,6 +3,11 @@ var Task = React.createClass({
     var className = this.props.day === 'everyday' ? '' : 'special-task';
     return (
       <li className={ className }>
+      	{
+      	  this.props.day === 'everyday' ? '' : (
+            <i className="material-icons">warning</i>
+          )
+      	}
         <span className="task-body">{ this.props.task }</span>
       </li>
     );
