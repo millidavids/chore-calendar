355b98ae0f512a3aa5b741c38c3c8a29a39efe1e
Sarah Vessels <cheshire137@gmail.com>
2015-11-13 23:15:17 -0500

    Add Task component

--- app/assets/javascripts/components/_chore_list.js.jsx
+++ app/assets/javascripts/components/_chore_list.js.jsx
@@ -1,30 +1,44 @@
 var ChoreList = React.createClass({
+  getInitialState: function() {
+    return {
+      tasks: {
+        everyday: [
+          'Gather dishes and trash from desks and downstairs table',
+          'Empty, load, and start dishwasher',
+          'Wipe countertops',
+          'Slay ants',
+          'Empty trash',
+          'Empty recycling',
+          'Check soap and toilet paper in bathrooms'
+        ],
+        monday: [
+          'Take trash cans to the curb'
+        ],
+        tuesday: [
+          'Roll in trash cans'
+        ],
+        wednesday: [],
+        thursday: [],
+        friday: [],
+        weekend: []
+      }
+    };
+  },
 
   render: function() {
     return (
-      <div className="chore-list">
-        <ul>
-          {
-            this.props.day === 'monday' ? (
-              <li className="special-task">Take trash cans to the curb</li>
-            ) : ''
-          }
-          {
-            this.props.day === 'tuesday' ? (
-              <li className="special-task">Roll in trash cans</li>
-            ) : ''
-          }
-          <li>Gather dishes and trash from desks and downstairs table</li>
-          <li>Empty dishwasher</li>
-          <li>Fill dishwasher</li>
-          <li>Start dishwasher</li>
-          <li>Wipe countertops</li>
-          <li>Slay ants</li>
-          <li>Empty trash</li>
-          <li>Empty recycling</li>
-          <li>Check soap and toilet paper in bathrooms</li>
-        </ul>
-      </div>
+      <ul className="chore-list">
+        {this.state.tasks[this.props.day].map(function(task) {
+          return (
+            <Task task={ task } day={ this.props.day } key={'task-' + task}/>
+          );
+        }.bind(this))}
+        {this.state.tasks.everyday.map(function(task) {
+          return (
+            <Task task={ task } day="everyday" key={'task-' + task}/>
+          );
+        })}
+      </ul>
     );
   }
 });
