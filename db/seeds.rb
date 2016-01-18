01cca1c762bc2eb7641808896638ba041d23fe8e
David Yurek <david.a.yurek@gmail.com>
2016-01-18 10:05:54 -0500

    change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,13 @@ puts "\tSeeded user #{user.username} with password #{ENV['DEFAULT_PASSWORD']}"
 puts 'Seeding people'
 user.people.create!(name: 'David Y')
 user.people.create!(name: 'Dave H')
-user.people.create!(name: 'Chase S')
-user.people.create!(name: 'Chase J')
 user.people.create!(name: 'Todd')
-sarah = user.people.create!(name: 'Sarah')
-user.people.create!(name: 'Nick')
 user.people.create!(name: 'Lindsay')
 user.people.create!(name: 'Summer')
 user.people.create!(name: 'Mklbtz')
-user.people.create!(name: 'Michael H')
-user.people.create!(name: 'Gorm')
-user.people.create!(name: 'Tito')
-user.people.create!(name: 'Erik')
+user.people.create!(name: 'Steeev')
+user.people.create!(name: 'Andrew')
 
 puts 'Seeding calendar'
 calendar = Calendar.create!(date: Date.today, current_day_id: 1, user: user,
                             person_order: [*1..user.people.count])
-
-puts 'Seeding exemptions'
-Exemption.create!(day: 'wednesday', person: sarah, calendar: calendar, recurring: true)
